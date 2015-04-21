a216734c907b77fdd5695ec1d8ecbb2c67c41664
Jon Edwards <jkedwards@me.com>
2015-04-21 13:03:04 -0700

    add ux

--- client/index.js
+++ client/index.js
@@ -22,6 +22,31 @@ angular.module('bank-account', ['firebase'])
     $scope.withdrawals.push({ amount: $scope.amount, date: new Date() });
     overdraftStatus();
   };
+  $scope.undoDeposit = function(){
+
+
+  };
+  $scope.undoWithdrawal = function(){
+
+  };
+  $scope.undoFee = function(){
+
+  };
+  $scope.undo = function(transactionType){
+    switch (transactionType){
+      case 'deposit':
+        $rootScope.balance -= this.amount;
+        $scope.deposits.splice(this.$index, 1);
+        break;
+      case 'withdrawal':
+        $rootScope.balance += this.amount;
+        $scope.withdrawals.splice(this.$index, 1);
+        break;
+      case 'fee':
+        $rootScope.balance += 50;
+        $scope.fees.splice(this.$index, 1);
+    }
+  }
   function overdraftStatus(){
     if($rootScope.balance < 0 && $rootScope.overdrafted === false){
       $scope.fees.push({ amount: 50, date: new Date() });
