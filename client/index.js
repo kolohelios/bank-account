99ed75adf4f7df98a1e2c6b3d8297eb36cfb94e9
Jon Edwards <jkedwards@me.com>
2015-04-21 11:20:48 -0700

    add basic functionality and ledger

--- client/index.js
+++ client/index.js
@@ -1,1 +1,34 @@
-angular.module('poseidon', ['firebase']);
+'use strict';
+
+angular.module('bank-account', ['firebase'])
+.run(['$rootScope', function($rootScope){
+  $rootScope.balance = 10000;
+  $rootScope.name = 'Bob Smith';
+  $rootScope.overdrafted = false;
+
+}])
+.controller('master', ['$rootScope', '$scope', function($rootScope, $scope){
+  $scope.deposits = [];
+  $scope.withdrawals = [];
+  $scope.fees = [];
+
+  $scope.deposit = function(){
+    $rootScope.balance += $scope.amount;
+    $scope.deposits.push({ amount: $scope.amount, date: new Date() });
+    overdraftStatus();
+  };
+  $scope.withdraw = function(){
+    $rootScope.balance -= $scope.amount;
+    $scope.withdrawals.push({ amount: $scope.amount, date: new Date() });
+    overdraftStatus();
+  };
+  function overdraftStatus(){
+    if($rootScope.balance < 0 && $rootScope.overdrafted === false){
+      $scope.fees.push({ amount: 50, date: new Date() });
+      $rootScope.balance -= 50;
+      $rootScope.overdrafted = true;
+    } else if($rootScope.balance > 0 && $rootScope.overdrafted === true){
+      $rootScope.overdrafted = false;
+    }
+  }
+}]);
